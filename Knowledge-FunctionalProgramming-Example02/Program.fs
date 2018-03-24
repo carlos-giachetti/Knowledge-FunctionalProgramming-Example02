ccad3bbc212a8007bf0c0262a821fb0035c23f41
carlos-giachetti <carlos_giachetti@hotmail.com>
2018-03-24 07:44:51 -0300

    Initial version created on Visual Studio 2017

---
+++ Knowledge-FunctionalProgramming-Example02/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+
+let square n = n * n;
+
+
+let sumOfSquaresOf10FirstIntegers = 
+    [1..10]
+    |> List.map square
+    |> List.sum
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Sum of squares of the first ten integer numbers = %i" sumOfSquaresOf10FirstIntegers
+
+    Console.ReadKey() |> ignore
+
+    0 // return an integer exit code
